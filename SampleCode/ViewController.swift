5fde6ae5708975a83643376f8a7b78ebc8738a18
kudpig <shin.kudo.aa@gmail.com>
2021-02-28 18:38:19 +0900

    [add]UIパーツの関連付け2

--- SampleCode/ViewController.swift
+++ SampleCode/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstItemLabel: UILabel!
+    
+    @IBOutlet weak var secondItemLabel: UILabel!
+    
+    @IBOutlet weak var totalPriceLabel: UILabel!
+    
+    @IBOutlet weak var firstSelectButton: UIButton!
+    
+    @IBOutlet weak var secondSelectButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func tapFirstSelectButton(_ sender: UIButton) {
+    }
+    
 
+    @IBAction func tapSecondSelectButton(_ sender: UIButton) {
+    }
+    
 }
 
