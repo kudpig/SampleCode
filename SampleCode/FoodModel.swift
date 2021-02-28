ddf020162b0d37f08f41dd0cc7e34e2cc4a18f1b
kudpig <shin.kudo.aa@gmail.com>
2021-02-28 19:53:23 +0900

    [add]FoodModelを定義&デコード設定

--- SampleCode/FoodModel.swift
+++ SampleCode/FoodModel.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+// Foodを定義
+struct Food: Codable {
+    
+    let name: String
+    let price: Int
+    let image: String
+    
+}
+
+let jsonStr = """
+    [
+        { "name": "Apple",      "price": 200,   "image": "Apple" },
+        { "name": "Orange",     "price": 120,   "image": "Orange" },
+        { "name": "Banana",     "price": 65,    "image": "Banana" },
+        { "name": "Grape",      "price": 400,   "image": "Grape" },
+        { "name": "Strawberry", "price": 600,   "image": "Strawberry" },
+        { "name": "Chocolate",  "price": 100,   "image": "Chocolate" },
+        { "name": "Candy",      "price": 20,    "image": "Candy" },
+        { "name": "Biscuit",    "price": 70,    "image": "Biscuit" },
+        { "name": "Water",      "price": 100,   "image": "Water" },
+        { "name": "Tea",        "price": 200,   "image": "Tea" },
+        { "name": "Cola",       "price": 150,   "image": "Cola" },
+        { "name": "Beer",       "price": 300,   "image": "Beer" },
+        { "name": "Wine",       "price": 500,   "image": "Wine" }
+    ]
+"""
+
+let jsonData = jsonStr.data(using: .utf8)  // json文字列をデータ型に変換
+// 受け取ったdataをJSONDecoder()のdecodeメソッドでswiftの形式に変換(struct) 変換先は配列型なので[Food].selfとしている
+let foods = try! JSONDecoder().decode([Food].self, from: jsonData!)
+
