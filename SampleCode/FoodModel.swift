162e755092ddc5dd5dfdedb5d7292aa990c5da08
kudpig <shin.kudo.aa@gmail.com>
2021-03-20 15:50:27 +0900

    [fix]コードレビュー受けた箇所の一部

--- SampleCode/FoodModel.swift
+++ SampleCode/FoodModel.swift
@@ -14,27 +14,51 @@ struct Food: Codable {
     let price: Int
     let image: String
     
+    static func createEmpty() -> Food {
+        return Food(name: "", price: 0, image: "")
+    }
+    
+    static func createDefaultFoods() -> [Food] {
+
+        let arr = [
+            Food(name: "Apple", price: 200, image: "Apple"),
+            Food(name: "Orange", price: 120 ,image: "Orange"),
+            Food(name: "Banana", price: 65 ,image: "Banana"),
+            Food(name: "Grape", price: 400 ,image: "Grape"),
+            Food(name: "Strawberry", price: 600 ,image: "Strawberry"),
+            Food(name: "Chocolate", price: 100 ,image: "Chocolate"),
+            Food(name: "Candy", price: 20 ,image: "Candy"),
+            Food(name: "Biscuit", price: 70 ,image: "Biscuit"),
+            Food(name: "Water", price: 100 ,image: "Water"),
+            Food(name: "Tea", price: 200 ,image: "Tea"),
+            Food(name: "Cola", price: 150 ,image: "Cola"),
+            Food(name: "Beer", price: 300 ,image: "Beer"),
+            Food(name: "Wine", price: 500 ,image: "Wine")
+        ]
+
+        return arr
+    }
+    
 }
 
-let jsonStr = """
-    [
-        { "name": "Apple",      "price": 200,   "image": "Apple" },
-        { "name": "Orange",     "price": 120,   "image": "Orange" },
-        { "name": "Banana",     "price": 65,    "image": "Banana" },
-        { "name": "Grape",      "price": 400,   "image": "Grape" },
-        { "name": "Strawberry", "price": 600,   "image": "Strawberry" },
-        { "name": "Chocolate",  "price": 100,   "image": "Chocolate" },
-        { "name": "Candy",      "price": 20,    "image": "Candy" },
-        { "name": "Biscuit",    "price": 70,    "image": "Biscuit" },
-        { "name": "Water",      "price": 100,   "image": "Water" },
-        { "name": "Tea",        "price": 200,   "image": "Tea" },
-        { "name": "Cola",       "price": 150,   "image": "Cola" },
-        { "name": "Beer",       "price": 300,   "image": "Beer" },
-        { "name": "Wine",       "price": 500,   "image": "Wine" }
-    ]
-"""
-
-let jsonData = jsonStr.data(using: .utf8)  // json文字列をデータ型に変換
-// 受け取ったdataをJSONDecoder()のdecodeメソッドでswiftの形式に変換(struct) 変換先は配列型なので[Food].selfとしている
-let foods = try! JSONDecoder().decode([Food].self, from: jsonData!)
-
+//let jsonStr = """
+//    [
+//        { "name": "Apple",      "price": 200,   "image": "Apple" },
+//        { "name": "Orange",     "price": 120,   "image": "Orange" },
+//        { "name": "Banana",     "price": 65,    "image": "Banana" },
+//        { "name": "Grape",      "price": 400,   "image": "Grape" },
+//        { "name": "Strawberry", "price": 600,   "image": "Strawberry" },
+//        { "name": "Chocolate",  "price": 100,   "image": "Chocolate" },
+//        { "name": "Candy",      "price": 20,    "image": "Candy" },
+//        { "name": "Biscuit",    "price": 70,    "image": "Biscuit" },
+//        { "name": "Water",      "price": 100,   "image": "Water" },
+//        { "name": "Tea",        "price": 200,   "image": "Tea" },
+//        { "name": "Cola",       "price": 150,   "image": "Cola" },
+//        { "name": "Beer",       "price": 300,   "image": "Beer" },
+//        { "name": "Wine",       "price": 500,   "image": "Wine" }
+//    ]
+//"""
+//
+//let jsonData = jsonStr.data(using: .utf8)  // json文字列をデータ型に変換
+//// 受け取ったdataをJSONDecoder()のdecodeメソッドでswiftの形式に変換(struct) 変換先は配列型なので[Food].selfとしている
+//let foods = try! JSONDecoder().decode([Food].self, from: jsonData!)
--- SampleCode/ViewController.swift
+++ SampleCode/ViewController.swift
@@ -9,49 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // 押されたボタンをcaseで分ける
-    enum WhichBtn: String {
-        case none = "noneBtn"
-        case first = "firstBtn"
-        case second = "secondBtn"
-    }
+    private lazy var foods = Food.createDefaultFoods()
     
-    // 一番直近のタップされたボタンを判別する変数
-    var nowButton: WhichBtn = .none // 初期値はボタンを押していないのでcase none
-    // 選択されたセルのrowを取得
-    var firstItemRowNumber: Int? = nil
-    var secondItemRowNumber: Int? = nil
     // Label 初期値
     var itemLabelPlaceholder: String = "選択されていません"
+    //FIX nameを表示するラベルを配列にする(上記の２つ)
+    private lazy var labels = [firstItemLabel, secondItemLabel]
+    //FIX 選択したものを配列にしている。初期値は全てEmptyとなっている
+    private lazy var selectFoods = [Food](repeating: Food.createEmpty(), count: labels.count)
+    
+    // 押されたボタンの状態管理
+    enum WhichBtn: Int {
+        case first = 0, second = 1, none = 2
+    }
+    var nowButton: WhichBtn = .none
+    
     
     // １つ目のラベル
     @IBOutlet weak var firstItemLabel: UILabel! {
         didSet {
-            // tableViewCellから値を受け取っていた場合にtextの内容を変える処理
-            if let rowNum = firstItemRowNumber {
-                firstItemLabel.text = foods[rowNum].name
-            } else {
-                firstItemLabel.text = itemLabelPlaceholder
-            }
+            firstItemLabel.text = itemLabelPlaceholder
         }
     }
     // ２つ目のラベル
     @IBOutlet weak var secondItemLabel: UILabel! {
         didSet {
-            if let rowNum = secondItemRowNumber {
-                secondItemLabel.text = foods[rowNum].name
-            } else {
-                secondItemLabel.text = itemLabelPlaceholder
-            }
+            secondItemLabel.text = itemLabelPlaceholder
         }
     }
     // ３つ目のラベル(TotalPrice)
     @IBOutlet weak var totalPriceLabel: UILabel! {
         didSet {
             // 合計金額の算出のためsumPriceメソッドを実行、引数として選択されたcellRowを渡す。関数内でアンラップするのでnilでも問題ない
-            totalPriceLabel.text = "合計 \(sumPrice(firstRowNum: firstItemRowNumber, secondRowNum: secondItemRowNumber)) 円"
+            totalPriceLabel.text = "○○○円"
         }
     }
+    
+    
+    
+    
     // １つ目のボタン
     @IBOutlet weak var firstSelectButton: UIButton! {
         didSet {
@@ -64,49 +60,49 @@ class ViewController: UIViewController {
             secondSelectButton.layer.cornerRadius = 40
         }
     }
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
         
-    }
-
-    
-    
-    // totalPrice計算メソッド
-    func sumPrice(firstRowNum: Int?, secondRowNum: Int?) -> Int {
-        
-        let firstPrice: Int
-        let secondPrice: Int
-        
-        // if letを使用し、nilの場合は0を代入する
-        if let rowNum = firstRowNum {
-            firstPrice = foods[rowNum].price
-        } else {
-            firstPrice = 0
-        }
-        
-        if let rowNum = secondRowNum {
-            secondPrice = foods[rowNum].price
-        } else {
-            secondPrice = 0
-        }
-        
-        return firstPrice + secondPrice
-    }
-    
-    
     // ButtonAciton Funcs
     // 画面遷移の前に押したボタン変数を更新する
     @IBAction func tapFirstSelectButton(_ sender: UIButton) {
         nowButton = .first
+        let vc = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(identifier: "ItemListViewController") as! ItemListViewController
+        vc.delegate = self
+        print(vc.delegate)
         performSegue(withIdentifier: "goItemList", sender: nil)
     }
     @IBAction func tapSecondSelectButton(_ sender: UIButton) {
         nowButton = .second
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "ItemListViewController") as! ItemListViewController
+        vc.delegate = self
         performSegue(withIdentifier: "goItemList", sender: nil)
     }
     
 }
 
+
+extension ViewController: ToPassItemProtocol {
+    func selectedItem(cellItem: Food) {
+        print("実行されました")
+    }
+    
+    
+    // cellがタップされるとselectedItemのfoodが上書きされ、ViewControllerの持つ値が更新される
+    //func selectedItem(cellItem: Food) {
+    //    print("selectedItemを実行しています")
+    //
+    //    switch nowButton {
+    //    // ボタンの状態がfirstもしくはsecondだった場合
+    //    case .first, .second:
+    //        // labelsに格納されている配列の順番と、nowButtonのenumのIntを一致させている
+    //        labels[nowButton.rawValue]?.text = cellItem.name
+    //        print("labelsにfoodnameを代入します")
+    //        // 選択されたFoodオブジェクトを配列に格納する(結果画面に渡せるように)
+    //        selectFoods[nowButton.rawValue] = cellItem
+    //    default:
+    //        break
+    //    }
+    //}
+    
+    
+    
+}
