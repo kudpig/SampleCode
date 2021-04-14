b10493b99f62b5700723b22c615844f47425911f
kudpig <shin.kudo.aa@gmail.com>
2021-04-15 03:42:15 +0900

    [add]Routerメソッドの作成

--- SampleCode/AppDelegate.swift
+++ SampleCode/AppDelegate.swift
@@ -13,20 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         
         let window = UIWindow(frame: UIScreen.main.bounds)
         self.window = window
-        
-        //Router.showRoot(window: window)
-        
-        // storyboardを取得
-        let storyboard = UIStoryboard(name: "Home", bundle: nil)
-        let vc = storyboard.instantiateInitialViewController() as! HomeViewController
-        // ナビゲーションコントローラーを定義。引数で最下層のページを指定
-        let nav = UINavigationController(rootViewController: vc)
-        window.rootViewController = nav
-        window.makeKeyAndVisible()
+        Router.showRoot(window: window)
         
         return true
     }
--- SampleCode/Router.swift
+++ SampleCode/Router.swift
@@ -5,4 +5,34 @@
 //  Created by Shinichiro Kudo on 2021/04/14.
 //
 
-import Foundation
+import UIKit
+
+final class Router {
+    
+    static func showRoot(window: UIWindow?) {
+        let storyboard = UIStoryboard(name: "Home", bundle: nil)
+        guard let vc = storyboard.instantiateInitialViewController() else {
+            return
+        }
+        let nav = UINavigationController(rootViewController: vc)
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
+    }
+    
+    static func showList(fromVC: UIViewController) {
+        let vc = UIStoryboard(name: "ListTable", bundle: nil).instantiateViewController(identifier: "ListTable") as! ListTableViewController
+        vc.delegate = fromVC as? ToPassDataProtocol
+        if let nav = fromVC.navigationController {
+            nav.pushViewController(vc, animated: true)
+        } else {
+            fromVC.present(vc, animated: true, completion: nil)
+        }
+    }
+    
+    // 今後確認するべき内容
+    // Routerファイルにdelegateを書いてもよいのか
+    // vc.delegate = fromVC as? ToPassDataProtocol について
+    // ToPassDataProtocolへのダウンキャスト？これは準拠(継承)させているということなのか？
+    // Cannot assign value of type 'HomeViewController' to type 'ToPassDataProtocol?'を解消している？
+    
+}
