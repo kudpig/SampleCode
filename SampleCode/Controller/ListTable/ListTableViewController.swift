9a0ddee23eb43acd223f1ace9ba157be829e9fc6
kudpig <shin.kudo.aa@gmail.com>
2021-04-30 16:53:01 +0900

    [add]セグメントによるフィルターの追加

--- SampleCode/Controller/ListTable/ListTableViewController.swift
+++ SampleCode/Controller/ListTable/ListTableViewController.swift
@@ -9,7 +9,41 @@ import UIKit
 
 class ListTableViewController: UIViewController {
     
+    @IBOutlet private weak var segmentControl: UISegmentedControl! {
+        didSet {
+            let segmentArray = Item.createSegmentArray()
+            segmentWithArray(array: segmentArray)
+            segmentControl.selectedSegmentIndex = .zero
+            segmentControl.selectedSegmentTintColor = .systemBlue
+            
+            segmentControl.addTarget(self, action: #selector(handleSegmentChage), for: .valueChanged)
+        }
+    }
+    
+    private func segmentWithArray(array: [String]) {
+        segmentControl.removeAllSegments()
+        for (index, string) in array.enumerated() {
+            segmentControl.insertSegment(withTitle: string, at: index, animated: false)
+        }
+    }
+    
+    @objc private func handleSegmentChage() {
+        
+        guard segmentControl.selectedSegmentIndex != .zero else {
+            displayItemList = itemList
+            tableView.reloadData()
+            return
+        }
+        
+        let filterList = itemList.filter { v in v.type == Item.genre.allCases[segmentControl.selectedSegmentIndex] }
+        displayItemList = filterList
+        tableView.reloadData()
+    }
+    
+    
     private lazy var itemList = Item.createDefaultItems()
+    
+    private lazy var displayItemList = itemList
     
     weak var delegate: ToPassDataProtocol?
     
@@ -29,7 +63,7 @@ class ListTableViewController: UIViewController {
 extension ListTableViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemList.count
+        return displayItemList.count
     }
     
     // ②cellの中身に関する設定
@@ -39,7 +73,7 @@ extension ListTableViewController: UITableViewDelegate, UITableViewDataSource {
             fatalError()
         }
         
-        let item = itemList[indexPath.row]
+        let item = displayItemList[indexPath.row]
         cell.configure(item: item)
         
         return cell
@@ -48,7 +82,7 @@ extension ListTableViewController: UITableViewDelegate, UITableViewDataSource {
     // ③セルをタップした時の設定
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        let item = itemList[indexPath.row]
+        let item = displayItemList[indexPath.row]
         delegate?.cellDidSelect(cellData: item)
         self.navigationController?.popViewController(animated: true)
     }
--- SampleCode/Model/ItemModel.swift
+++ SampleCode/Model/ItemModel.swift
@@ -9,35 +9,43 @@ import Foundation
 
 struct Item {
     
+    let type: genre
     let name: String
     let price: Int
     let image: String
     
-    //var isEmpty: Bool
+    enum genre: String, CaseIterable {
+        case all = "All", fruits = "fruits", sweets = "sweets", drinks = "drink", none = ""
+    }
     
     static func createEmpty() -> Item {
-        return Item(name: "", price: 0, image: "")
+        return Item(type: .none, name: "", price: 0, image: "")
     }
     
     static func createDefaultItems() -> [Item] {
 
         let arr = [
-            Item(name: "Apple", price: 200, image: "Apple"),
-            Item(name: "Orange", price: 120 ,image: "Orange"),
-            Item(name: "Banana", price: 65 ,image: "Banana"),
-            Item(name: "Grape", price: 400 ,image: "Grape"),
-            Item(name: "Strawberry", price: 600 ,image: "Strawberry"),
-            Item(name: "Chocolate", price: 100 ,image: "Chocolate"),
-            Item(name: "Candy", price: 20 ,image: "Candy"),
-            Item(name: "Biscuit", price: 70 ,image: "Biscuit"),
-            Item(name: "Water", price: 100 ,image: "Water"),
-            Item(name: "Tea", price: 200 ,image: "Tea"),
-            Item(name: "Cola", price: 150 ,image: "Cola"),
-            Item(name: "Beer", price: 300 ,image: "Beer"),
-            Item(name: "Wine", price: 500 ,image: "Wine")
+            Item(type: .fruits, name: "Apple", price: 200, image: "Apple"),
+            Item(type: .fruits, name: "Orange", price: 120 ,image: "Orange"),
+            Item(type: .fruits, name: "Banana", price: 65 ,image: "Banana"),
+            Item(type: .fruits, name: "Grape", price: 400 ,image: "Grape"),
+            Item(type: .fruits, name: "Strawberry", price: 600 ,image: "Strawberry"),
+            Item(type: .sweets, name: "Chocolate", price: 100 ,image: "Chocolate"),
+            Item(type: .sweets, name: "Candy", price: 20 ,image: "Candy"),
+            Item(type: .sweets, name: "Biscuit", price: 70 ,image: "Biscuit"),
+            Item(type: .drinks, name: "Water", price: 100 ,image: "Water"),
+            Item(type: .drinks, name: "Tea", price: 200 ,image: "Tea"),
+            Item(type: .drinks, name: "Cola", price: 150 ,image: "Cola"),
+            Item(type: .drinks, name: "Beer", price: 300 ,image: "Beer"),
+            Item(type: .drinks, name: "Wine", price: 500 ,image: "Wine")
         ]
 
         return arr
+    }
+    
+    static func createSegmentArray() -> [String] {
+        let arrStr = genre.allCases.filter { genre in !genre.rawValue.isEmpty }.map { (arr) -> String in arr.rawValue }
+        return arrStr
     }
     
 }
