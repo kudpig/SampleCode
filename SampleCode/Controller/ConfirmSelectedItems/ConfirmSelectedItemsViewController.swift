b5dff5ad382ae7da01215a3d2c9e5592f0c0e1a5
kudpig <shin.kudo.aa@gmail.com>
2021-05-04 19:07:42 +0900

    [add]クラスのカプセル化

--- SampleCode/Controller/ConfirmSelectedItems/ConfirmSelectedItemsViewController.swift
+++ SampleCode/Controller/ConfirmSelectedItems/ConfirmSelectedItemsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ConfirmSelectedItemsViewController: UIViewController {
+final class ConfirmSelectedItemsViewController: UIViewController {
     
     var receiveItems: [Item] = []
     
